cd6f3ff502a3dcac73d5829572af3b618f785955
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-08-11 16:52:15 +0200

    adding test for invalid login

--- kushchenkoProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ kushchenkoProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,12 +1,13 @@
 package loginTests;
+
 import baseTest.BaseTest;
 import org.junit.Assert;
 import org.junit.Test;
 
 
-public class LoginTestWithPageObject  extends BaseTest {
+public class LoginTestWithPageObject extends BaseTest {
     @Test
-    public void TR001_validLogin(){
+    public void TR001_validLogin() {
         pageProvider.getLoginPage().openLoginPage();
         pageProvider.getLoginPage().enterTextIntoInputLogin("qaauto");
         pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
@@ -14,5 +15,20 @@ public class LoginTestWithPageObject  extends BaseTest {
 
         Assert.assertTrue("Button Sign Out is not visible",
                 pageProvider.getHomePage().isButtonSignOutVisible());
+    }
+
+    @Test
+    public void TR002_invalidLogin() {
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin("1qaauto");
+        pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+
+        Assert.assertFalse("Sign Out button is visible",
+                pageProvider.getHomePage().isButtonSignOutVisible());
+        Assert.assertTrue("Sign In button is not visible",
+                pageProvider.getLoginPage().isButtonSignInVisible());
+        Assert.assertTrue("Alert message is not displayed",
+                pageProvider.getLoginPage().isAlertMessageVisible());
     }
 }
