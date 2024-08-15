2834bdd1096099ecf0185d8cd808cfe9d0049e73
Alex Maslovsky <maslovsky_ab@groupbwt.com>
2024-08-15 21:51:45 +0300

    test create post - part2

--- maslovskyProj/src/main/java/pages/PostPage.java
+++ maslovskyProj/src/main/java/pages/PostPage.java
@@ -1,15 +1,50 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.elements.HeaderElement;
 
 public class PostPage extends ParentPage {
+    @FindBy(xpath = ".//div[@class='alert alert-success text-center']")
+    private WebElement successMessage;
+
+    @FindBy(xpath = ".//button[@class='delete-post-button text-danger']")
+    private WebElement buttonDeletePost;
+
     public PostPage(WebDriver webDriver) {
         super(webDriver);
+    }
+
+    public HeaderElement getHeaderElement() {
+        return new HeaderElement(webDriver);
     }
 
     public PostPage checkIsRedirectToPostPage() {
         //TODO: check URL
         //TODO: check some element
         return this;
+    }
+
+    /**
+     * Method checks if success message is displayed
+     * doesn't check the text of the message
+     * @return PostPage
+     */
+    public PostPage checkIsSuccessMessageDisplayed() {
+        Assert.assertTrue("Success message is not displayed", isElementVisible(successMessage));
+        return this;
+    }
+
+    public PostPage checkTextInSuccessMessage(String expectedMessageText) {
+        String actualText = successMessage.getText();
+        Assert.assertEquals("Message text is not as expected", expectedMessageText, actualText);
+        return this;
+    }
+
+    public MyProfilePage clickOnDeleteButton() {
+        clickOnElement(buttonDeletePost);
+        return new MyProfilePage(webDriver);
     }
 }
--- maslovskyProj/src/test/java/postTests/CreateNewPostTest.java
+++ maslovskyProj/src/test/java/postTests/CreateNewPostTest.java
@@ -1,9 +1,14 @@
 package postTests;
 
 import baseTest.BaseTest;
+import org.junit.After;
 import org.junit.Test;
+import utils.Utils;
 
 public class CreateNewPostTest extends BaseTest {
+    //GUID = 1b1b1b1b-1b1b-1b1b-1b1b-1b1b1b1b1b1b
+    private final String POST_TITLE = "TR_003_mine_" + Utils.getDateAndTimeFormatted();
+
     @Test
     public void TR003_createNewPost() {
         pageProvider.getLoginPage()
@@ -11,11 +16,26 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectOnHomePage()
                 .clickOnButtonCreatePost()
                 .checkIsRedirectOnCreateNewPostPage()
-                .enterTextIntoInputTitle("Title of the post from me")
+                .enterTextIntoInputTitle(POST_TITLE)
                 .enterTextIntoTextAreaBody("Body of the post from me")
+                // .setCheckBox to ON (ask what is your state, if ON, do nothing if OFF, click) - add these methods to CommonActionsWithElements
                 .clickOnButtonSaveNewPost()
                 .checkIsRedirectToPostPage()
-        ;
+                .checkIsSuccessMessageDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
+                // isThisPost unique& : yes
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE, 1);
+    }
+
+    @After
+    public void deletePost() {
+        pageProvider.getHomePage()
+                .openHomePageAndLoginIfNeeded()
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .deletePostsTillPresent(POST_TITLE);
     }
 
 }
