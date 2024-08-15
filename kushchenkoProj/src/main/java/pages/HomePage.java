1439e3c69edeb14a28de03ce30af30514b212558
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-08-15 20:58:05 +0200

    adding test for post creation part2

--- kushchenkoProj/src/main/java/pages/HomePage.java
+++ kushchenkoProj/src/main/java/pages/HomePage.java
@@ -1,14 +1,16 @@
 package pages;
 
+import data.TestData;
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import pages.elements.HeaderElement;
 
 public class HomePage extends ParentPage {
+    Logger logger = Logger.getLogger(getClass());
 
-    @FindBy(xpath = "//button[text()='Sign Out']")
-    private WebElement buttonSignOut;
 
     @FindBy(xpath = "//a[@class='btn btn-sm btn-success mr-2']")
     private WebElement buttonCreatePost;
@@ -17,12 +19,13 @@ public class HomePage extends ParentPage {
         super(webDriver);
     }
 
-    public boolean isButtonSignOutVisible() {
-        return isElementVisible(buttonSignOut);
+    public HeaderElement getHeaderElement() {
+        return new HeaderElement(webDriver);
     }
 
+
     public HomePage checkIsRedirectToHomePage() {
-        Assert.assertTrue("It is not Home page", isButtonSignOutVisible());
+        Assert.assertTrue("It is not Home page", getHeaderElement().isButtonSignOutVisible());
         //TODO checkUrl
         return this;
     }
@@ -30,5 +33,20 @@ public class HomePage extends ParentPage {
     public CreateNewPostPage clickOnButtonCreatePost() {
         clickOnElement(buttonCreatePost);
         return new CreateNewPostPage(webDriver);
+    }
+
+    public HomePage openHomePageAndLoginIfNeeded() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.openLoginPage();
+        if (getHeaderElement().isButtonSignOutVisible()) {
+            logger.info("User is already logged in");
+        } else {
+            loginPage.enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
+            loginPage.enterTextIntoInputPassword(TestData.VALID_PASSWORD_UI);
+            loginPage.clickOnButtonSignIn();
+            checkIsRedirectToHomePage();
+            logger.info("User was logged in");
+        }
+        return this;
     }
 }
--- kushchenkoProj/src/main/java/pages/ParentPage.java
+++ kushchenkoProj/src/main/java/pages/ParentPage.java
@@ -1,8 +1,10 @@
 package pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 public class ParentPage  extends CommonActionsWithElements{
+    protected Logger logger = Logger.getLogger(getClass());
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
