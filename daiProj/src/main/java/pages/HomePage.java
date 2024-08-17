9f9e86e8117ba77e2747f1d00796e5f771a4442f
mashiioo <tkhitkhuinha@MacBook-Pro.local>
2024-08-17 21:01:50 +0300

    test for Create post part 2

--- daiProj/src/main/java/pages/HomePage.java
+++ daiProj/src/main/java/pages/HomePage.java
@@ -1,25 +1,30 @@
 package pages;
 
+import data.TestData;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;;
+import org.openqa.selenium.support.FindBy;
+import pages.elements.HeaderElement;;
 
 public class HomePage extends ParentPage{
 
+    Logger logger = Logger.getLogger(getClass());
     @FindBy(xpath = "//button[text()='Sign Out']")
     private WebElement buttonSignOut;
-
-    private Logger logger = Logger.getLogger(getClass());
 
     @FindBy(xpath = "//a[@class='btn btn-sm btn-success mr-2']")
     private WebElement buttonCreatePost;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
+    }
+
+    public HeaderElement getHeaderElement(){
+        return new HeaderElement(webDriver);
     }
 
     // public boolean isButtonSignOutVisible() {
@@ -45,5 +50,21 @@ public class HomePage extends ParentPage{
     public CreateNewPostPage clickOnButtonCreatePost() {
         clickOnElement(buttonCreatePost);
         return new CreateNewPostPage(webDriver);
+    }
+
+    public HomePage openHomePageAndLoginIfNeeded() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.openLoginPage();
+        if (isButtonSignOutVisible()) {
+            logger.info("User is already logged in");
+        } else {
+            loginPage.enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
+            loginPage.enterTextIntoInputPassword(TestData.VALID_PASSWORD_UI);
+            loginPage.clickOnButtonSignIn();
+            checkIsRedirectToHomePage();
+            logger.info("User was logged in")
+;
+        }
+        return this;
     }
 }
--- daiProj/src/test/java/postTests/CreateNewPostTest.java
+++ daiProj/src/test/java/postTests/CreateNewPostTest.java
@@ -1,17 +1,45 @@
 package postTests;
 
 import baseBase.BaseTest;
+import org.junit.After;
 import org.junit.Test;
+import utils.Utils;
 
 public class CreateNewPostTest extends BaseTest {
+    // GUID = 1b1b1b1b-1b1b-1b1b-1b1b-1b1b1b1b1b1b
+    private final String POST_TITLE = "dai " + Utils.getDateAndTimeFormatted();
     @Test
     public void TR_003_createNewPost(){
         pageProvider.getLoginPage().openLoginPageAndFillLoginFormWithValidCred()
                 .checkIsRedirectToHomePage().clickOnButtonCreatePost()
                 .checkIsRedirectToCreateNewPostPage()
-                .enterTextIntoInputTitle("Title of the post from Masha")
-                .enterTextIntoInputBody("Body of the post from Masha")
+                .enterTextIntoInputTitle(POST_TITLE)
+                .enterTextIntoInputBody("Body of the post from Masha ")
+                // setcheckbox to true
                 .clickOnButtonSaveNewPost()
-                .checkIsRedirectToPostPage();
+                .checkIsRedirectToPostPage()
+                .checkIsSuccessMessageDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
+                //is this post unique? : yes
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE, 1)
+
+
+
+        ;
     }
+@After
+    public void deletePosts(){
+        pageProvider.getHomePage()
+                .openHomePageAndLoginIfNeeded()
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .deletePostsTillPresent(POST_TITLE)
+
+
+
+        ;
+    }
+
 }
