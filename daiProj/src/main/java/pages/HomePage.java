9db4a8e1fb8e6df9d14d45281e34fa9580724705
mashiioo <tkhitkhuinha@MacBook-Pro.local>
2024-08-17 21:23:28 +0300

    button Sign Out was moved to HeaderElement

--- daiProj/src/main/java/pages/HomePage.java
+++ daiProj/src/main/java/pages/HomePage.java
@@ -13,8 +13,7 @@ import pages.elements.HeaderElement;;
 public class HomePage extends ParentPage{
 
     Logger logger = Logger.getLogger(getClass());
-    @FindBy(xpath = "//button[text()='Sign Out']")
-    private WebElement buttonSignOut;
+
 
     @FindBy(xpath = "//a[@class='btn btn-sm btn-success mr-2']")
     private WebElement buttonCreatePost;
@@ -37,12 +36,9 @@ public class HomePage extends ParentPage{
     //  return false;
     // }
 
-    public boolean isButtonSignOutVisible() {
-        return isElementVisible(buttonSignOut);
-}
 
     public HomePage checkIsRedirectToHomePage() {
-        Assert.assertTrue("It is not Home page", isButtonSignOutVisible());
+        Assert.assertTrue("It is not Home page", getHeaderElement().isButtonSignOutVisible());
         //TODO checkUrl
         return this;
     }
@@ -55,7 +51,7 @@ public class HomePage extends ParentPage{
     public HomePage openHomePageAndLoginIfNeeded() {
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.openLoginPage();
-        if (isButtonSignOutVisible()) {
+        if (getHeaderElement().isButtonSignOutVisible()) {
             logger.info("User is already logged in");
         } else {
             loginPage.enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
