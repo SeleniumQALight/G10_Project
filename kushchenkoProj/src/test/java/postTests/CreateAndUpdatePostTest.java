c4799db70b04162fd360972be5aeaedb832c68cb
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-08-29 16:18:22 +0200

    added deleting post if it wasn't updated

--- kushchenkoProj/src/test/java/postTests/CreateAndUpdatePostTest.java
+++ kushchenkoProj/src/test/java/postTests/CreateAndUpdatePostTest.java
@@ -43,6 +43,7 @@ public class CreateAndUpdatePostTest extends BaseTest {
                 .getHeaderElement().clickOnMyProfileButton()
                 .checkIsRedirectToProfilePage()
                 .deletePostsTillPresent(UPDATED_POST_TITLE)
+                .deletePostsTillPresent(POST_TITLE)
         ;
     }
 }
