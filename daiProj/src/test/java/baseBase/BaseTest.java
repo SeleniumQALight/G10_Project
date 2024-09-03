f662863294f341f1c3430647b3269d47afb1dfae
mashiioo <tkhitkhuinha@MacBook-Pro.local>
2024-09-03 21:39:49 +0300

    browser

--- daiProj/src/test/java/baseBase/BaseTest.java
+++ daiProj/src/test/java/baseBase/BaseTest.java
@@ -6,6 +6,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import pages.PageProvider;
 
 import java.time.Duration;
@@ -19,8 +23,9 @@ public class BaseTest {
       //this method will be run before each test method
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        webDriver = new ChromeDriver();
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         logger.info("Browser was opened");
@@ -28,10 +33,36 @@ public class BaseTest {
 
     }
 
+
     @After
     public void tearDown() {
         webDriver.quit();
         logger.info("Browser was closed");
 
+    }
+
+    private WebDriver initDriver() {
+        String browserFromProperty = System.getProperty("browser");
+        if ((browserFromProperty == null) || (browserFromProperty.equalsIgnoreCase("chrome"))) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+            logger.info("Browser is chrome");
+        } else if (browserFromProperty.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        } else if ("ie".equals(browserFromProperty.toLowerCase())){
+            WebDriverManager.iedriver().setup(); //zoom 100%
+            webDriver = new InternetExplorerDriver(); //security level - Medium
+        } else if ("safari".equalsIgnoreCase(browserFromProperty)) {
+            WebDriverManager.safaridriver().setup();
+            webDriver = new SafariDriver();
+        } else if ("edge".equalsIgnoreCase(browserFromProperty)) {
+           WebDriverManager.edgedriver().setup();
+            webDriver = new EdgeDriver();
+        } else {
+            throw new IllegalArgumentException("Browser " + browserFromProperty + " is not supported");
+        }
+
+        return webDriver;
     }
 }
