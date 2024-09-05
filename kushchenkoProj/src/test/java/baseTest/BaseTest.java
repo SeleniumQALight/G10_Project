c64a64ad5e8ee727f9fab7035e870d90d61923bf
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-09-05 14:35:06 +0200

    checkIsElementVisible was added, params, envs, browsers

--- kushchenkoProj/src/test/java/baseTest/BaseTest.java
+++ kushchenkoProj/src/test/java/baseTest/BaseTest.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import pages.PageProvider;
 
 import java.time.Duration;
@@ -20,8 +22,9 @@ public class BaseTest {
 
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        webDriver = new ChromeDriver();
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         logger.info("Browser is opened");
@@ -32,5 +35,24 @@ public class BaseTest {
     public void tearDown() {
         webDriver.quit();
         logger.info("Browser is closed");
+    }
+
+    private WebDriver initDriver() {
+        String browserFromProperty = System.getProperty("browser");
+        logger.info("Browser from property: " + browserFromProperty);
+        if ((browserFromProperty == null) || (browserFromProperty.equalsIgnoreCase("chrome"))) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+            logger.info("Chrome browser is started");
+        } else if (browserFromProperty.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        } else if (browserFromProperty.equalsIgnoreCase("safari")) {
+            WebDriverManager.getInstance(SafariDriver.class).setup();
+            webDriver = new SafariDriver();
+        } else {
+            throw new IllegalArgumentException("Browser " + browserFromProperty + " is not supported");
+        }
+        return webDriver;
     }
 }
