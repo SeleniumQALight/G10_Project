3e540bc5861285ccdcc0298aa88611bea5c010bf
andrejostaleckij <olpakmgtd@gmail.com>
2024-09-14 10:14:22 +0300

    CW12

---
+++ ostaletskiyProj/src/test/java/database/DataBaseTest.java
@@ -0,0 +1,30 @@
+package database;
+
+import Pages.MyProfilePage;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import utils.Database;
+
+public class DataBaseTest {
+    private Database mySQLDataBase;
+    private Logger logger = Logger.getLogger(getClass());
+
+
+    @Before
+    public void setMySQLDataBase(){
+
+    }
+
+    @After
+    public void tearDownMySQLDataBase(){
+
+    }
+
+    @Test
+    public void testDataFromDB (){
+
+    }
+
+}
