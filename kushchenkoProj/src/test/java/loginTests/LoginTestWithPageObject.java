ac374f5b48e16e95515dc5cf56f0316f54d40b1a
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-09-15 10:58:26 +0300

    suits were added

--- kushchenkoProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ kushchenkoProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,11 +1,13 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import categories.SmokeTestFilter;
 import data.TestData;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import utils.ConfigProvider;
 import utils.ExcelDriver;
@@ -16,11 +18,11 @@ import java.util.Map;
 import static data.TestData.*;
 
 
-@RunWith(JUnitParamsRunner.class)
 public class LoginTestWithPageObject extends BaseTest {
     final String ALERT_MESSAGE = "Invalid username/password.";
 
     @Test
+    @Category(SmokeTestFilter.class)
     public void TR001_validLogin() {
         pageProvider.getLoginPage().openLoginPage();
         pageProvider.getLoginPage().enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
@@ -124,23 +126,5 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.getCommonActionsWithElements().refreshPage();
         pageProvider.getLoginPage().clickOnButtonSignIn();
         pageProvider.getLoginPage().checkIsButtonSignInVisible();
-    }
-
-    @Test
-    @Parameters(method = "parametersForInvalidLoginTest")
-    public void TR010_invalidLoginWithParameters(String login, String password, String alertMessage) {
-        pageProvider.getLoginPage().openLoginPage();
-        pageProvider.getLoginPage().enterTextIntoInputLogin(login);
-        pageProvider.getLoginPage().enterTextIntoInputPassword(password);
-        pageProvider.getLoginPage().clickOnButtonSignIn();
-        pageProvider.getLoginPage().checkIsAlertMessageDisplayed(alertMessage);
-    }
-
-    public Object[][] parametersForInvalidLoginTest() {
-        return new Object[][]{
-                {INVALID_LOGIN_UI, INVALID_PASSWORD_UI, ALERT_MESSAGE},
-                {VALID_LOGIN_UI, INVALID_PASSWORD_UI, ALERT_MESSAGE},
-                {INVALID_LOGIN_UI, VALID_PASSWORD_UI, ALERT_MESSAGE}
-        };
     }
 }
