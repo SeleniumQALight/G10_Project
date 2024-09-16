bd183b2928e75995988d8af3e1cb48174611c17b
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-09-16 11:00:09 +0300

    test name

--- kushchenkoProj/src/test/java/baseTest/BaseTest.java
+++ kushchenkoProj/src/test/java/baseTest/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -18,6 +20,7 @@ public class BaseTest {
     private WebDriver webDriver;
     private Logger logger = Logger.getLogger(getClass());
     protected PageProvider pageProvider;
+    final String symbols = "-".repeat(10);
 
 
     // цей блок виконується перед кожним тестом (аналог BeforeEach в JUnit 5)
@@ -26,6 +29,7 @@ public class BaseTest {
     public void setup() {
 //        WebDriverManager.chromedriver().setup();
 //        webDriver = new ChromeDriver();
+        logger.info(symbols + testName.getMethodName() + " was started " + symbols);
         webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(configProperties.TIME_FOR_IMPLICIT_WAIT()));
@@ -37,7 +41,11 @@ public class BaseTest {
     public void tearDown() {
         webDriver.quit();
         logger.info("Browser is closed");
+        logger.info(symbols + testName.getMethodName() + " was finished " + symbols);
     }
+
+    @Rule
+    public TestName testName = new TestName();
 
     private WebDriver initDriver() {
         String browserFromProperty = System.getProperty("browser");
