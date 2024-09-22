deb12b9360c74ba34c3544ab1c2a2fcd6603a44e
andrejostaleckij <olpakmgtd@gmail.com>
2024-09-22 18:06:26 +0300

    FIX 1

--- ostaletskiyProj/src/main/java/Pages/elements/HeaderElement.java
+++ ostaletskiyProj/src/main/java/Pages/elements/HeaderElement.java
@@ -27,6 +27,12 @@ public class HeaderElement extends CommonActionsWithElements {
     @FindBy(xpath = "//span[@class='text-white mr-2 header-chat-icon']")
     private WebElement chatButton;
 
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    private WebElement inputUserNameInLoginForm;
+
+    @FindBy(xpath = "//input[@placeholder='Password']")
+    private WebElement inputPasswordInLoginForm;
+
 
     public HeaderElement(WebDriver webDriver) {
         super(webDriver);
@@ -71,5 +77,12 @@ public class HeaderElement extends CommonActionsWithElements {
     public CreateNewPostPage clickOnButtonCreatePost() {
         clickOnElement(buttonCreatePost);
         return new CreateNewPostPage(webDriver);
+    }
+
+    public WebElement getInputUserNameInLoginForm() {
+        return inputUserNameInLoginForm;
+    }
+    public WebElement getInputPasswordInLoginForm() {
+        return inputPasswordInLoginForm;
     }
 }
