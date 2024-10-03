1eab32659db7ae5b6b4f738e3eb163b1a17a9c86
amaslovsky <a.maslovsky@gmail.com>
2024-10-03 21:23:26 +0300

    CW2 - API test 2

--- maslovskyProj/src/test/java/apiTests/ApiTests.java
+++ maslovskyProj/src/test/java/apiTests/ApiTests.java
@@ -1,7 +1,12 @@
 package apiTests;
 
 import api.EndPoints;
+import api.dto.responseDto.AuthorDto;
+import api.dto.responseDto.PostsDto;
 import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,10 +15,15 @@ import static org.hamcrest.CoreMatchers.everyItem;
 
 public class ApiTests {
     final String USER_NAME = "autoapi";
+    Logger logger = Logger.getLogger(getClass());
 
     @Test
     public void getAllPostsForUser() {
-        given()
+        PostsDto[] actualResponseAsDto =
+                given()
+////////////////////////////////////////////////////////////////
+//                        .queryParam("", "")
+////////////////////////////////////////////////////////////////
                 .contentType(ContentType.JSON)
                 .log().all()
                 .when()
@@ -21,10 +31,40 @@ public class ApiTests {
                 .then()
                 .log().all()
                 .statusCode(200)
+        // method #1 restassured asserts
                 .body("[0].title", equalTo("The second Default post"))
                 .body("author.username", everyItem(equalTo(USER_NAME)))
+        //method #2 DTO
+                .extract().body().as(PostsDto[].class)
         ;
+        logger.info("Number of posts = " + actualResponseAsDto.length);
+        logger.info("Title = " + actualResponseAsDto[0].getTitle());
+        logger.info("User Name = " + actualResponseAsDto[0].getAuthor().getUsername());
+        logger.info(actualResponseAsDto[1]);
 
+        for (int i = 0; i < actualResponseAsDto.length; i++) {
+            Assert.assertEquals("UserName is not expected in post " + i
+            , USER_NAME
+            , actualResponseAsDto[i].getAuthor().getUsername());
+        }
+
+        //Expected result
+        PostsDto[] expectedResponseDto = {
+                new PostsDto("The second Default post", "This post was created automatically after cleaning the database",
+                        "All Users", "no", new AuthorDto(USER_NAME), false),
+                new PostsDto("The first Default post", "This post was created automatically after cleaning the database",
+                        "All Users", "no", new AuthorDto(USER_NAME), false)
+        };
+
+        Assert.assertEquals("Number of posts ", expectedResponseDto.length, actualResponseAsDto.length);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions
+                .assertThat(actualResponseAsDto)
+                        .usingRecursiveComparison()
+                                .ignoringFields("id", "createdDate", "author.avatar")
+                                        .isEqualTo(expectedResponseDto);
+        softAssertions.assertAll();
     }
 
 }
