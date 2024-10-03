dcfed78a21d27b8605c44c3d22ebd93d1f27b628
Kateryna Pervushyna <shegyak@gmail.com>
2024-10-03 21:23:26 +0300

    api test 2

---
+++ pervushynaProj/src/main/java/api/dto/responseDto/PostsDto.java
@@ -0,0 +1,105 @@
+package api.dto.responseDto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PostsDto {
+    @JsonProperty("_id")
+    private String id;
+    private String title;
+    private String body;
+    private String select1;
+    private String uniquePost;
+    private String createdDate;
+    private AuthorDto author;
+    public Boolean isVisitorOwner;
+
+    public PostsDto() {
+    }
+
+    public PostsDto(String title, String body, String select1, String uniquePost, AuthorDto author, Boolean isVisitorOwner) {
+        this.title = title;
+        this.body = body;
+        this.select1 = select1;
+        this.uniquePost = uniquePost;
+        this.author = author;
+        this.isVisitorOwner = isVisitorOwner;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSelect1() {
+        return select1;
+    }
+
+    public void setSelect1(String select1) {
+        this.select1 = select1;
+    }
+
+    public String getUniquePost() {
+        return uniquePost;
+    }
+
+    public void setUniquePost(String uniquePost) {
+        this.uniquePost = uniquePost;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public AuthorDto getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDto author) {
+        this.author = author;
+    }
+
+    public Boolean getIsVisitorOwner() {
+        return isVisitorOwner;
+    }
+
+    public void setIsVisitorOwner(Boolean visitorOwner) {
+        isVisitorOwner = visitorOwner;
+    }
+
+    @Override
+    public String toString() {
+        return "PostsDto{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", select1='" + select1 + '\'' +
+                ", uniquePost='" + uniquePost + '\'' +
+                ", createDate='" + createdDate + '\'' +
+                ", author=" + author +
+                ", isVisitorOwner=" + isVisitorOwner +
+                '}';
+    }
+}
