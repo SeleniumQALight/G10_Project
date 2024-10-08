3e3088b306fd3099d46f6c4c4d2cd7c796ab2796
tradulenko <radulenko@gmail.com>
2024-10-08 20:53:50 +0300

    jSong path

--- overAll/src/main/java/api/ApiHelper.java
+++ overAll/src/main/java/api/ApiHelper.java
@@ -2,12 +2,13 @@ package api;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
 
 import static api.EndPoints.POSTS_BY_USER;
 import static io.restassured.RestAssured.given;
 
 public class ApiHelper {
-    public ValidatableResponse getAllPostByUserRequest(String userName, int expectedStatusCode){
+    public ValidatableResponse getAllPostByUserRequest(String userName, int expectedStatusCode) {
         return given()
                 .contentType(ContentType.JSON)
                 .log().all()
@@ -16,5 +17,9 @@ public class ApiHelper {
                 .then()
                 .log().all()
                 .statusCode(expectedStatusCode);
+    }
+
+    public ValidatableResponse getAllPostByUserRequest(String userName) {
+        return getAllPostByUserRequest(userName, HttpStatus.SC_OK);
     }
 }
--- overAll/src/test/java/apiTests/ApiTests.java
+++ overAll/src/test/java/apiTests/ApiTests.java
@@ -5,10 +5,14 @@ import api.EndPoints;
 import api.dto.responseDto.AuthorDto;
 import api.dto.responseDto.PostsDto;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -99,7 +103,8 @@ public class ApiTests {
 
        String actualResponse =
                apiHelper.getAllPostByUserRequest(NOT_VALID_USER_NAME, SC_BAD_REQUEST)
-               .extract().response().body().asString();
+                       // method #3 response as String
+                       .extract().response().body().asString();
 
        Assert.assertEquals(
                "Message in response "
@@ -107,6 +112,34 @@ public class ApiTests {
                        +" or there is no posts. Exception is undefined\""
                ,actualResponse
        );
+    }
+
+    @Test
+    public void getAllPostsByUserPath(){
+        // method 4 - json path
+        Response actualResponse = apiHelper.getAllPostByUserRequest(USER_NAME).extract().response();
+
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        List<String> actualListOfPosts = actualResponse.jsonPath().getList("title", String.class);
+        logger.info(actualListOfPosts);
+
+        for (int i = 0; i < actualListOfPosts.size(); i++) {
+            softAssertions.assertThat(actualListOfPosts.get(i))
+                    .as("Item number " + i)
+                    .contains("Default post");
+        }
+
+        List<Map> actualAuthorList = actualResponse.jsonPath().getList("author", Map.class);
+
+        for (Map actualAuthorObject: actualAuthorList){
+            softAssertions.assertThat(actualAuthorObject.get("username"))
+                    .as("Field userName in Author")
+                    .isEqualTo(USER_NAME);
+        }
+        softAssertions.assertAll();
+
     }
 
 
