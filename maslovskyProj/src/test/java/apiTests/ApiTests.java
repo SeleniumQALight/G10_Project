581401bc5bd44881a68325723658f52598c812de
amaslovsky <a.maslovsky@gmail.com>
2024-10-08 21:24:27 +0300

    schema

--- maslovskyProj/src/test/java/apiTests/ApiTests.java
+++ maslovskyProj/src/test/java/apiTests/ApiTests.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
@@ -136,6 +137,16 @@ public class ApiTests {
         }
 
         softAssertions.assertAll();
+    }
+
+    @Test
+    public void getAllPostsByUserSchema() {
+        //method #5 - JSON schema
+        apiHelper
+                .getAllPostByUserRequest(USER_NAME)
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("response.json"));
+
     }
 
 }
