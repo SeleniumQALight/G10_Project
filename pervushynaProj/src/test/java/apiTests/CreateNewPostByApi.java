8268c9f70a66edbeba51283a4bb81da42d5cbc89
Kateryna Pervushyna <shegyak@gmail.com>
2024-10-13 15:53:22 +0300

    1

---
+++ pervushynaProj/src/test/java/apiTests/CreateNewPostByApi.java
@@ -0,0 +1,17 @@
+package apiTests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CreateNewPostByApi {
+
+    @Before
+    public void getTokenAndDeletePosts() {
+
+    }
+
+    @Test
+    public void createNewPostByApi() {
+
+    }
+}
