298b340c3d491131bd85be0332b124fbf2a6352e
Harbar Nataliia <nharbar@mgb.org>
2024-10-15 19:45:06 +0300

    Allure + RestAssured

--- harbarProj/src/main/java/api/ApiHelper.java
+++ harbarProj/src/main/java/api/ApiHelper.java
@@ -2,6 +2,7 @@ package api;
 
 import api.dto.responseDto.PostsDto;
 import data.TestData;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
@@ -23,6 +24,7 @@ public class ApiHelper {
     private Logger logger = Logger.getLogger(getClass());
 
     public static RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .addFilter(new AllureRestAssured())
             .setContentType(JSON)
             .log(LogDetail.ALL)
             .build();
