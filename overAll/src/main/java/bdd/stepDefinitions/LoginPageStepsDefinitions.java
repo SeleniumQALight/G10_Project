1113cfce0b8b6e941e23f4584b4204c59d4ca3d4
tradulenko <radulenko@gmail.com>
2024-10-15 21:26:13 +0300

    bdd test 1

---
+++ overAll/src/main/java/bdd/stepDefinitions/LoginPageStepsDefinitions.java
@@ -0,0 +1,17 @@
+package bdd.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPageStepsDefinitions {
+    @Given("I open Login page")
+    public void iOpenLoginPage(){
+        // TODO
+    }
+
+
+    @When("I login with valid cred")
+    public void i_login_with_valid_cred() {
+        //TODO
+    }
+}
