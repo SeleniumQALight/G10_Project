d6cc0fb57260f3ba282136b631e42582776b89ad
tradulenko <radulenko@gmail.com>
2024-10-17 20:48:35 +0300

    bdd test 1 was impl

--- overAll/src/main/java/bdd/stepDefinitions/LoginPageStepsDefinitions.java
+++ overAll/src/main/java/bdd/stepDefinitions/LoginPageStepsDefinitions.java
@@ -1,17 +1,26 @@
 package bdd.stepDefinitions;
 
+import bdd.helpers.WebDriverHelper;
+import data.TestData;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
-public class LoginPageStepsDefinitions {
+public class LoginPageStepsDefinitions extends MainSteps {
+
+    public LoginPageStepsDefinitions(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
     @Given("I open Login page")
     public void iOpenLoginPage(){
-        // TODO
+        pageProvider.getLoginPage().openLoginPage();
     }
 
 
     @When("I login with valid cred")
     public void i_login_with_valid_cred() {
-        //TODO
+        pageProvider.getLoginPage().enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
+        pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.VALID_PASSWORD_UI);
+        pageProvider.getLoginPage().clickOnButtonSignIn();
     }
 }
