c11b27b769949ef4e8844a54f1747a643edf7893
amaslovsky <a.maslovsky@gmail.com>
2024-10-17 20:48:25 +0300

    BDD test 1 was impl

---
+++ maslovskyProj/src/main/java/bdd/stepDefinitions/LoginPageStepsDefinitions.java
@@ -0,0 +1,25 @@
+package bdd.stepDefinitions;
+
+import bdd.helpers.WebDriverHelper;
+import data.TestData;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPageStepsDefinitions extends MainSteps {
+    public LoginPageStepsDefinitions(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
+    @Given("I open Login page")
+    public void iOpenLoginPage() {
+        pageProvider.getLoginPage().openLoginPage();
+    }
+
+    @When("I login with valid cred")
+    public void i_login_with_valid_cred() {
+        pageProvider.getLoginPage().enterTextIntoInputLogin(TestData.VALID_LOGIN_UI);
+        pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.VALID_PASSWORD_UI);
+        pageProvider.getLoginPage().clickOnButtonSighIn();
+    }
+
+}
--- maslovskyProj/src/test/resources/features/login.feature
+++ maslovskyProj/src/test/resources/features/login.feature
@@ -1,5 +1,6 @@
 Feature: Login Feature
 
+  @R001
   Scenario: ROO1 Valid Login
     Given I open Login page
     When I login with valid cred
