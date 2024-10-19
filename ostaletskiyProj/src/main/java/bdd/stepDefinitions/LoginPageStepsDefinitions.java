4af9ba91b306b6a6b65cf6377f3d6a140be75d4d
andrejostaleckij <olpakmgtd@gmail.com>
2024-10-19 11:39:42 +0300

    Bdd start

---
+++ ostaletskiyProj/src/main/java/bdd/stepDefinitions/LoginPageStepsDefinitions.java
@@ -0,0 +1,22 @@
+package bdd.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageStepsDefinitions {
+    @Given("I open Login page")
+    public void iOpenLoginPage () {
+
+    }
+
+    @When("I login with valid cred")
+    public void i_login_with_valid_cred() {
+    }
+
+    @Then("I see avatar on Home page")
+    public void i_see_avatar_on_home_page() {
+    }
+
+
+}
