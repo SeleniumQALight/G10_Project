eddce47924aefd58bb2c45523f023497743e11c5
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-10-21 17:25:29 +0200

    bdd test 1 was implemented

--- kushchenkoProj/src/main/java/bdd/stepDefinitions/HomePageStepsDefinitions.java
+++ kushchenkoProj/src/main/java/bdd/stepDefinitions/HomePageStepsDefinitions.java
@@ -1,10 +1,15 @@
 package bdd.stepDefinitions;
 
+import bdd.helpers.WebDriverHelper;
 import io.cucumber.java.en.Then;
 
-public class HomePageStepsDefinitions {
+public class HomePageStepsDefinitions extends MainSteps {
+    public HomePageStepsDefinitions(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
     @Then("I see avatar on Home page")
     public void iSeeAvatarOnHomePage() {
-       //TODO
+        pageProvider.getHomePage().getHeaderElement().isButtonMyProfileVisible();
     }
 }
