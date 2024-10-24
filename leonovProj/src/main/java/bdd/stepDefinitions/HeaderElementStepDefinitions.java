946fba6d91218e128079eebeea8084feb6b893d9
Denys Leonov <skydark912@gmail.com>
2024-10-24 17:12:48 +0300

    CW7 test UI + API

---
+++ leonovProj/src/main/java/bdd/stepDefinitions/HeaderElementStepDefinitions.java
@@ -0,0 +1,17 @@
+package bdd.stepDefinitions;
+
+import bdd.helpers.WebDriverHelper;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HeaderElementStepDefinitions extends MainSteps{
+    public HeaderElementStepDefinitions(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+    @When("I click on button MyProfile on Header Element")
+    public void i_click_on_button_my_profile_on_header_element() {
+        pageProvider.getHomePage().getHeader().clickOnButtonMyProfile();
+
+    }
+
+}
