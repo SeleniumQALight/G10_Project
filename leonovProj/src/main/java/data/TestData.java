4020963b274a006009a0cc98adbbf2c66f9dcd82
Denys Leonov <skydark912@gmail.com>
2024-10-27 02:20:39 +0300

    HW2 the first part

--- leonovProj/src/main/java/data/TestData.java
+++ leonovProj/src/main/java/data/TestData.java
@@ -13,4 +13,7 @@ public class TestData {
     public static final String VALID_LOGIN_API = "denysleonov";
     public static final String VALID_PASSWORD_API = "123456qwerty";
 
+    public static final String VALID_LOGIN_BOOK = "denystest1";
+    public static final String VALID_PASSWORD_BOOK = "Qwerty123456!";
+
 }
