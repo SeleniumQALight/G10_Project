ec8392bbc757b631774c6dd7212b1fcedc83949f
Yuliia Kushchenko <yuliia.kushchenko@pitchbook.com>
2024-10-28 14:52:27 +0100

    test UI + API

--- kushchenkoProj/src/main/java/bdd/stepDefinitions/HomePageStepsDefinitions.java
+++ kushchenkoProj/src/main/java/bdd/stepDefinitions/HomePageStepsDefinitions.java
@@ -1,6 +1,8 @@
 package bdd.stepDefinitions;
 
 import bdd.helpers.WebDriverHelper;
+import data.TestData;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 
 public class HomePageStepsDefinitions extends MainSteps {
@@ -10,6 +12,21 @@ public class HomePageStepsDefinitions extends MainSteps {
 
     @Then("I see avatar on Home page")
     public void iSeeAvatarOnHomePage() {
-        pageProvider.getHomePage().getHeaderElement().isButtonMyProfileVisible();
+        pageProvider.getHomePage().getHeaderElement().checkIsMyProfileButtonVisible();
+    }
+
+    @And("I open Home page as {string} user and {string} password")
+    public void iOpenHomePageAsDefaultUserAndDefaultPassword(String username, String password) {
+        if (DEFAULT.equalsIgnoreCase(username)) {
+            username = TestData.VALID_LOGIN_API;
+        }
+        if (DEFAULT.equalsIgnoreCase(password)) {
+            password = TestData.VALID_PASSWORD_API;
+        }
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin(username);
+        pageProvider.getLoginPage().enterTextIntoInputPassword(password);
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+        pageProvider.getHomePage().checkIsRedirectToHomePage();
     }
 }
--- kushchenkoProj/src/main/java/bdd/stepDefinitions/MainSteps.java
+++ kushchenkoProj/src/main/java/bdd/stepDefinitions/MainSteps.java
@@ -7,11 +7,10 @@ public class MainSteps {
     protected WebDriverHelper webDriverHelper;
     protected PageProvider pageProvider;
 
+    public static final String DEFAULT = "default";
+
     public MainSteps(WebDriverHelper webDriverHelper) {
         this.webDriverHelper = webDriverHelper;
         pageProvider = new PageProvider(webDriverHelper.getWebDriver());
-
     }
-
-
 }
