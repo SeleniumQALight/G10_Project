2229f1e67168f44387042c1985b225fd31aef174
Kateryna Pervushyna <shegyak@gmail.com>
2024-10-30 11:42:58 +0200

    bdd test 2 was impl

--- pervushynaProj/src/main/java/bdd/stepDefinitions/LoginPageStepsDefinitions.java
+++ pervushynaProj/src/main/java/bdd/stepDefinitions/LoginPageStepsDefinitions.java
@@ -3,6 +3,7 @@ package bdd.stepDefinitions;
 import bdd.helpers.WebDriverHelper;
 import data.TestData;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginPageStepsDefinitions extends MainSteps{
@@ -24,4 +25,22 @@ public class LoginPageStepsDefinitions extends MainSteps{
 
     }
 
+    @When("I enter {string} into input Login on Login page")
+    public void iEnterIntoInputLoginOnLoginPage(String login) {
+        pageProvider.getLoginPage().enterTextIntoInputLogin(login);
+    }
+
+    @When("I enter {string} into input Password on Login page")
+    public void i_enter_into_input_password_on_login_page(String password) {
+      pageProvider.getLoginPage().enterTextIntoInputPassword(password);
+    }
+    @When("I click on button SignIn in Login page")
+    public void i_click_on_button_sign_in_in_login_page() {
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+    }
+
+    @Then("I see alert message with text {string}")
+    public void iSeeAlertMessageWithTextInvalidUsernamePassword(String expectedMessage) {
+        pageProvider.getLoginPage().checkTextInAlertMessageInCenter(expectedMessage);
+    }
 }
--- pervushynaProj/src/test/resources/features/login.feature
+++ pervushynaProj/src/test/resources/features/login.feature
@@ -5,3 +5,17 @@ Feature: Login Feature
     Given I open Login page
     When I login with valid cred
     Then I see avatar on Home page
+
+
+  @R002
+  Scenario Outline: R002 Login with invalid cred
+    Given I open Login page
+    When I enter '<login>' into input Login on Login page
+    And I enter '<password>' into input Password on Login page
+    And I click on button SignIn in Login page
+    Then I see alert message with text 'Invalid username/password.'
+
+    Examples:
+      | login           | password           |
+      | qaauto          | not_valid_password |
+      | not_valid_login | 123456qwerty       |
