b5106929877e719dafb86973bf7e7e588502384f
Kateryna Pervushyna <shegyak@gmail.com>
2024-11-07 20:31:49 +0200

    exam test Privat

--- pervushynaProj/src/main/java/data/TestDataPrivat.java
+++ pervushynaProj/src/main/java/data/TestDataPrivat.java
@@ -2,41 +2,11 @@ package data;
 
 
 public class TestDataPrivat {
-    private final String currency;
-    private final double apiBuyRate;
-    private final double apiSaleRate;
-    private final double uiBuyRate;
-    private final double uiSaleRate;
-
-    // Конструктор
-    public TestDataPrivat(String currency, double apiBuyRate, double apiSaleRate, double uiBuyRate, double uiSaleRate) {
-        this.currency = currency;
-        this.apiBuyRate = apiBuyRate;
-        this.apiSaleRate = apiSaleRate;
-        this.uiBuyRate = uiBuyRate;
-        this.uiSaleRate = uiSaleRate;
-    }
-
-    // Геттери для полів
-    public String getCurrency() {
-        return currency;
-    }
-
-    public double getApiBuyRate() {
-        return apiBuyRate;
-    }
-
-    public double getApiSaleRate() {
-        return apiSaleRate;
-    }
-
-    public double getUiBuyRate() {
-        return uiBuyRate;
-    }
-
-    public double getUiSaleRate() {
-        return uiSaleRate;
-    }
+    public static String currency;
+    public static double apiBuyRate;
+    public static double apiSaleRate;
+    public static double uiBuyRate;
+    public static double uiSaleRate;
 
 
 }
